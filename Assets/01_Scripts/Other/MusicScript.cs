cc13944d3019c5fcdc3113a1de8c9aefd05a3bea
Gonçalo Pinto <69385490+gsespinto@users.noreply.github.com>
2022-02-11 22:11:31 +0000

    Musics

---
+++ Assets/01_Scripts/Other/MusicScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(AudioSource))]
+public class MusicScript : MonoBehaviour
+{
+    Animator animator;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        foreach (MusicScript ms in GameObject.FindObjectsOfType<MusicScript>())
+        {
+            if (ms == this)
+                continue;
+
+            ms.FadeOut();
+        }
+
+        animator = this.GetComponent<Animator>();
+        audioSource = this.GetComponent<AudioSource>();
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void FadeOut()
+    {
+        if (!animator)
+        {
+            Debug.Log("Missing animator reference.", this);
+            Destroy(this);
+            return;
+        }
+
+        if (!audioSource)
+        {
+            Debug.Log("Missing audio source reference.", this);
+            Destroy(this);
+            return;
+        }
+
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void Destroy()
+    {
+        Destroy(this.gameObject);
+    }
+}
